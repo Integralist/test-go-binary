585d2f1a9897068c924ee8e12526cfce2d146e23
Integralist <mark.mcdx@gmail.com>
2024-02-16 13:39:22 +0000

    v1

--- main.go
+++ main.go
@@ -6,8 +6,10 @@ import (
 )
 
 func main() {
-	var msg string
+	var msg, warning string
 	flag.StringVar(&msg, "msg", "", "message to be displayed")
+	flag.StringVar(&warning, "warning", "", "warning to be displayed")
 	flag.Parse()
 	fmt.Printf("message: %+v\n", msg)
+	fmt.Printf("warning: %+v\n", warning)
 }
